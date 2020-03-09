8d6616a06dc015fe885f09ee510d60d8029d139c
Gene Rocha <gene@generocha.com>
2020-03-09 12:57:00 -0700

    Create Client.java

---
+++ Assignment9/Client.java
@@ -0,0 +1,17 @@
+
+public class Client {
+
+	public static void main(String[] args) {
+		Product cheese = new Product(1, "Fresh Mozzarella 6oz",5.95, 20);
+		Product tomatoes = new Product(2, "Organic Crushed Tomatoes 28oz", 2.49, 25);
+		Product anchovies = new Product(3, "Anchovies fillet in olive and salt 4.25oz", 3.95, 13);
+		System.out.println(cheese);
+		System.out.println();
+		System.out.println(tomatoes);
+		System.out.println();
+		System.out.println(anchovies);
+		
+
+	}
+
+}
