70f6ad99caf312b699afa5b13f18ce7b6148cf94
Gene Rocha <gene@generocha.com>
2020-03-15 17:43:07 -0700

    Create Ticket.java

---
+++ Assignment10/Ticket.java
@@ -0,0 +1,17 @@
+
+public abstract class Ticket {
+	//**************************************
+	//
+	// The Class: Ticket
+	//
+	// By:  Gene Rocha 
+	// Date: 03/15/2020
+	//
+	//**************************************
+	
+	public abstract int getTicketNumber();
+	
+	public abstract double getTicketPrice();
+	
+	public abstract String toString();
+}
