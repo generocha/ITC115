339d16036568d8dab4d981b480447411003aa8f2
Gene Rocha <gene@generocha.com>
2020-03-23 15:26:20 -0700

    Create Book.java

---
+++ final/Book.java
@@ -0,0 +1,57 @@
+import java.text.NumberFormat;
+
+public class Book extends LibraryItem{
+	
+	// fields
+	private int itemNumber = 0;
+	
+	private String title;
+	
+	private String reserved;
+	
+	private String checkedIn;
+	
+	private int daysLate;
+	
+	// constructor
+	
+	public Book(int itemNumber, String title, String reserved, String checkedIn, int daysLate) {
+		this.itemNumber = itemNumber;
+		this.title = title;
+		this.reserved = reserved;
+		this.checkedIn = checkedIn;
+		this.daysLate = daysLate;
+		this.fines = daysLate * .50;
+	}
+	double fines = 0.0;
+	
+	@Override
+	public int getItemNumber() {
+		return itemNumber;
+	}
+	@Override
+	public String title() {
+		return title;
+	}
+	@Override
+	public String getReserved() {
+		return reserved;
+	}
+	@Override
+	public String getCheckedIn() {
+		return checkedIn;
+	}
+	@Override
+	public int getDaysLate() {
+		return daysLate;
+	}
+	@Override
+	public double geFinesAccrued() {
+		return fines = daysLate * 50;
+	}
+	@Override
+    public String toString() {
+		return "Library Item Number: #" +  itemNumber + ", Title: " + title + ", Reserved: " 
+    + reserved + ", Checked In: " + checkedIn + ", Days Late: " + daysLate + ", Fines Accrued: $" + fines + " ";
+    }
+}
