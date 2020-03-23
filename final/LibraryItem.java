34ecb930dd957f8ccc88cd063a9221abfc50cb06
Gene Rocha <gene@generocha.com>
2020-03-23 15:25:48 -0700

    create LibraryItem.java

---
+++ final/LibraryItem.java
@@ -0,0 +1,17 @@
+
+public abstract class LibraryItem {
+	
+	public  abstract int getItemNumber();
+	
+	public abstract String title();
+	
+	public abstract String getReserved();
+	
+	public abstract String getCheckedIn();
+	
+	public abstract int getDaysLate();
+	
+	public abstract double geFinesAccrued();
+	
+	public abstract String toString();
+}
